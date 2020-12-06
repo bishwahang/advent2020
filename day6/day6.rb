cc5964ebfde633814e6c269f46e0fa282d00a66b
Bishwa Hang Rai <bishwahang.kirat@gmail.com>
2020-12-06 11:31:09 +0100

    day 6

---
+++ day6/day6.rb
@@ -0,0 +1,48 @@
+#!/usr/bin/env ruby
+
+class Answer
+  attr_reader :answer
+
+  def initialize(answer)
+    @answer = answer.chars
+  end
+end
+
+class Group
+  attr_reader :answers
+
+  def initialize(answers)
+    @answers = answers
+  end
+
+  def unique_union_count
+    answers.map {|answer| answer.answer}.reduce(&:|).count
+  end
+
+  def unique_intersection_count
+    answers.map {|answer| answer.answer}.reduce(&:&).count
+  end
+end
+
+
+inputs = File.readlines("input.txt").map(&:chomp)
+
+answers = []
+groups  = []
+
+inputs.each do |answer|
+  if answer.empty?
+    groups << Group.new(answers)
+    answers = []
+  else
+    answers << Answer.new(answer)
+  end
+end
+
+groups << Group.new(answers) unless answers.empty?
+
+puts "Part 1"
+p groups.map { |group| group.unique_union_count }.sum
+
+puts "Part 2"
+p groups.map { |group| group.unique_intersection_count }.sum
